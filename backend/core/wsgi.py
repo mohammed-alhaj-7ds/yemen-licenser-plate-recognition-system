bcb28b45e30bb7566c5688ba9743c85a81e23d17
Mohammed Alhaj <malborwa@gmail.com>
2026-01-27 05:37:06 +0300

    upload project files

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -4,6 +4,7 @@ WSGI config for LPR project.
 
 import os
 from django.core.wsgi import get_wsgi_application
+print("DEBUG: Loading core.wsgi application...")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.base')
 application = get_wsgi_application()
